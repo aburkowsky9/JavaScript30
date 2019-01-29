ebb33237ef2604d2ebbc297f92466b312d0d9a03
Alex Burkowsky <aburkowsky9@gmail.com>
2019-01-28 16:20:31 -0800

    add keycode element selection and corresponding wav fileplay

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -1,1 +1,26 @@
-console.log('hello');
+const allDrums = Array.from(document.getElementsByClassName('key'));
+
+document.addEventListener('keydown', (e) => {
+  const keyCode = e.keyCode.toString();
+  const drum = allDrums.filter((div) => div.attributes[0].value === keyCode);
+  if (drum.length > 0) {
+    manipulateClass(drum[0]);
+  }
+});
+
+const manipulateClass = function(drum) {
+  drum.classList.add('playing');
+  playSound(drum);
+  setTimeout(0, () => {
+    drum.classList.remove('playing');
+  });
+}
+
+const playSound = function(drum) {
+  const audioType = drum.querySelector('.sound').innerHTML;
+  const audio = new Audio(`./sounds/${audioType}.wav`);
+  audio.play();
+}
+
+// do not select a drum if not returned by filter
+// seperate out function into drum selection and sound playing
