efd3b0a9c5a1733641d8cbbafe6025b3744d74e0
Alex Burkowsky <aburkowsky9@gmail.com>
2019-02-04 13:23:04 -0800

    swap individual element selectors for nodelist from querySelectorAll, use forEach method to assign event listeners and use helper function to dynamic change css var values

--- 03 - CSS Variables/cssvars.js
+++ 03 - CSS Variables/cssvars.js
@@ -1,30 +1,48 @@
 const root = document.documentElement;
-const spacingSlider = document.getElementById('spacing');
-const blurSlider = document.getElementById('blur');
-const colorSelector = document.getElementById('base');
+const modifiers = document.querySelectorAll('.controls input');
 
-spacingSlider.addEventListener('change', () => {
-  root.style.setProperty('--spacing', spacingSlider.value + 'px');
-});
+function changeCSSValue(varName, varValue) {
+  if (varName === 'base') {
+    root.style.setProperty(`--${varName}`, varValue);
+  } else {
+    root.style.setProperty(`--${varName}`, `${varValue}px`);
+  }
+}
 
-spacingSlider.addEventListener('mousemove', () => {
-  root.style.setProperty('--spacing', spacingSlider.value + 'px');
+modifiers.forEach((node) => {
+  node.addEventListener('change', () => {
+    changeCSSValue(node.id, node.value);
+  });
+  node.addEventListener('mousemove', () => {
+    changeCSSValue(node.id, node.value);
+  });
 });
+// const spacingSlider = document.getElementById('spacing');
+// const blurSlider = document.getElementById('blur');
+// const colorSelector = document.getElementById('base');
 
-blurSlider.addEventListener('change', () => {
-  root.style.setProperty('--blur', blurSlider.value + 'px');
-});
+// spacingSlider.addEventListener('change', () => {
+//   root.style.setProperty('--spacing', spacingSlider.value + 'px');
+// });
 
-blurSlider.addEventListener('mousemove', () => {
-  root.style.setProperty('--blur', blurSlider.value + 'px');
-});
+// spacingSlider.addEventListener('mousemove', () => {
+//   root.style.setProperty('--spacing', spacingSlider.value + 'px');
+// });
 
-colorSelector.addEventListener('change', () => {
-  root.style.setProperty('--base', colorSelector.value);
-});
+// blurSlider.addEventListener('change', () => {
+//   root.style.setProperty('--blur', blurSlider.value + 'px');
+// });
 
-colorSelector.addEventListener('mousemove', () => {
-  root.style.setProperty('--base', colorSelector.value);
-});
+// blurSlider.addEventListener('mousemove', () => {
+//   root.style.setProperty('--blur', blurSlider.value + 'px');
+// });
+
+// colorSelector.addEventListener('change', () => {
+//   root.style.setProperty('--base', colorSelector.value);
+// });
+
+// colorSelector.addEventListener('mousemove', () => {
+//   root.style.setProperty('--base', colorSelector.value);
+// });
 
 
