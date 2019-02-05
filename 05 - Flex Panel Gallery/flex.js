eeb4d9f5cc7e18d2976b39e10abf0baad523db32
Alex Burkowsky <aburkowsky9@gmail.com>
2019-02-04 21:01:30 -0800

    refactor js css property changing handlers

--- 05 - Flex Panel Gallery/flex.js
+++ 05 - Flex Panel Gallery/flex.js
@@ -1,19 +1,26 @@
-const panels = document.querySelectorAll('.panel');
-panels.forEach((panel) => {
-  panel.addEventListener('click', () => {
-    if (panel.classList.contains('open')) {
-      panel.classList.remove('open');
-    } else {
-      panel.classList.add('open');
-    }
-  });
-  panel.addEventListener('transitionend', (event) => {
-    if (event.propertyName === 'flex' || event.propertyName === 'flex-grow') {
-      if (panel.classList.contains('follow-open')) {
-        panel.classList.remove('follow-open');
-      } else {
-        panel.classList.add('follow-open');
-      }
-    }
-  });
-}); 
+function handlePanelSizing() {
+  handleClassList('open', this);
+}
+
+function handleTextTransition(event) {
+  if (event.propertyName === 'flex' || event.propertyName === 'flex-grow') {
+    handleClassList('follow-open', this);
+  }
+}
+
+function handleClassList(className, node) {
+  if (node.classList.contains(className)) {
+    node.classList.remove(className);
+  } else {
+    node.classList.add(className);
+  }
+}
+
+(function() {
+  const panels = document.querySelectorAll('.panel');
+  
+  panels.forEach((panel) => {
+    panel.addEventListener('click', handlePanelSizing);
+    panel.addEventListener('transitionend', handleTextTransition);
+  }); 
+})();
