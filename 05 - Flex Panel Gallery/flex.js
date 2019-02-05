cbf61ffed0f95a539a09198fa0e192fc5a8e61fb
Alex Burkowsky <aburkowsky9@gmail.com>
2019-02-04 20:44:22 -0800

    refactor css to establish intuitive flexbox grid

--- 05 - Flex Panel Gallery/flex.js
+++ 05 - Flex Panel Gallery/flex.js
@@ -3,12 +3,17 @@ panels.forEach((panel) => {
   panel.addEventListener('click', () => {
     if (panel.classList.contains('open')) {
       panel.classList.remove('open');
-      panel.childNodes[1].style.transform = 'translateY(-100%)';
-      panel.childNodes[5].style.transform = 'translateY(100%)';
     } else {
       panel.classList.add('open');
-      panel.childNodes[1].style.transform = 'translateY(50%)';
-      panel.childNodes[5].style.transform = 'translateY(-50%)';
     }
-  })
+  });
+  panel.addEventListener('transitionend', (event) => {
+    if (event.propertyName === 'flex' || event.propertyName === 'flex-grow') {
+      if (panel.classList.contains('follow-open')) {
+        panel.classList.remove('follow-open');
+      } else {
+        panel.classList.add('follow-open');
+      }
+    }
+  });
 }); 
